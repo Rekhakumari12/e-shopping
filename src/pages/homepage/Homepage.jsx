9635c9c37129b072c16418eea5d8d68453d9bdb6
Rekhakumari12 <maneeta808@gmail.com>
2022-09-17 19:03:42 +0530

    broken homepage to smaller components & finished styling menu component

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menu from '../../components/menu/Menu'
+import './homepage.styles.scss'
+export default function Homepage() {
+  return (
+    <div className='homepage'>
+      <Menu />
+    </div>
+  )
+}
+
